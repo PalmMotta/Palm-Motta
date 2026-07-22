a65dc142eda07ccbf0561cf2ff0e47d8fc83f713
PalmMotta <mottaexecutive@gmail.com>
2026-07-22 15:39:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.querySelector(".enter-btn").addEventListener("click", function (e) {
+    e.preventDefault();
 
+    document.querySelector("#pm-os").scrollIntoView({
+        behavior: "smooth"
+    });
+});
